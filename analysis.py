c00a6936d5c8369906722df1f7c22ef94e4431e4
Marcin Kaminski <marcin442228@gmail.com>
2025-03-30 14:30:41 +0100

    Initial commit of analysis.py

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# analysis.py
+# Author: Marcin Kaminski
+"""
+This program will:
+1. Output a summary of each variable to a single text file, 
+2. Save a histogram of each variable to png files, and
+3. Output a scatter plot of each pair of variables.
+4. Perform any other analysis appropriate.
+"""
+
+# TBC
+
